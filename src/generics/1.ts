886f77e903eb71f2874a2ca866d5620bc812f5fb
denis-mahei <denmahei@gmail.com>
2025-06-16 09:16:05 +0300

    generics 1 done

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-async function fetchData(url) {
+async function fetchData<T>(url: string): Promise<T> {
   try {
     const response = await axios.get(url);
     return response.data;
-  } catch (error) {
+  } catch (error: unknown) {
     throw new Error(`Error fetching from ${url}: ${error}`);
   }
 }
